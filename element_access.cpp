fbd5448996243e4ed2e026a2298afb41ac334935
Md. Jaynal Abdin <mjabdin.311@gmail.com>
2024-10-31 13:55:04 +0600

    Element access in string added

---
+++ element_access.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    // cout<<"Enter your string: "<<endl;
+    cin >> s;
+    cout << s << endl;
+    cout << s.at(0) << endl;
+    cout << s[s.size() - 1] << endl;
+    cout << s[3] << endl;
+    cout << s.back() << endl;
+    cout << s.front() << endl;
+
+    return 0;
+}

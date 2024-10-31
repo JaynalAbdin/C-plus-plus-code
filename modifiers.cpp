d5c5de1200d1f60239ec4c0da562140ae89a65f6
Md. Jaynal Abdin <mjabdin.311@gmail.com>
2024-10-31 14:18:43 +0600

    String modifiers added

---
+++ modifiers.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1;
+    string s2;
+    cin >> s1;
+    cin >> s2;
+
+    // s1 += s2;
+    // s1.append(s2);
+    // cout << s1 << endl;
+    // cout << s2 << endl;
+    s1.push_back('A');
+    cout << s1 << endl;
+    s2.pop_back();
+    cout << s2 << endl;
+
+    return 0;
+}

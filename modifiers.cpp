db58454a04ae204af6c3902a0a2d1c034025d7ab
Md. Jaynal Abdin <mjabdin.311@gmail.com>
2024-10-31 16:17:07 +0600

    Iterator added

--- modifiers.cpp
+++ modifiers.cpp
@@ -4,18 +4,21 @@ using namespace std;
 int main()
 {
     string s1;
-    string s2;
+    // string s2;
     cin >> s1;
-    cin >> s2;
+    // cin >> s2;
 
     // s1 += s2;
     // s1.append(s2);
     // cout << s1 << endl;
     // cout << s2 << endl;
-    s1.push_back('A');
+    // s1.push_back('A');
+    // cout << s1 << endl;
+    // s2.pop_back();
+    // cout << s2 << endl;
+    // s1.erase(3, 3);
+    s1.replace(3, 3, "and");
     cout << s1 << endl;
-    s2.pop_back();
-    cout << s2 << endl;
 
     return 0;
 }

977c4b1ff1ca42dc3a2a45862e4d7b4c57b59e68
Md. Jaynal Abdin <mjabdin.311@gmail.com>
2024-11-03 23:29:12 +0600

    String Constructor added

---
+++ palindrom1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s << endl;
+    string rv = s;
+    reverse(rv.begin(), rv.end());
+    count << rv << endl;
+
+    return 0;
+}
---
+++ string_constructor.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s("Jaynal Abdin");
+    // string s("Jaynal Abdin", 4);
+    // string a = "Jaynal Abdin";
+    // string s(a, 4);
+    string s(10, 'F');
+    cout << s << endl;
+
+    return 0;
+}

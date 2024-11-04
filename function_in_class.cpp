d3ea65813cf518f74a85014bfe0df67cf90e8a2e
Md. Jaynal Abdin <mjabdin.311@gmail.com>
2024-11-04 13:50:03 +0600

    total marks added

--- function_in_class.cpp
+++ function_in_class.cpp
@@ -6,16 +6,24 @@ class Person
 public:
     string name;
     int age;
-    Person(string nm, int ag)
+    int marks1, marks2;
+    Person(string nm, int ag, int mr1, int mr2)
     {
         name = nm;
         age = ag;
+        marks1 = mr1;
+        marks2 = mr2;
+    }
+    int total_marks()
+    {
+        return marks1 + marks2;
     }
 };
 
 int main()
 {
-    Person student("Jaynal Abdin", 28);
-    cout << student.name << " " << student.age;
+    Person student("Jaynal Abdin", 28, 85, 45);
+    cout << student.name << " " << student.age << endl;
+    cout << "Total Marks: " << student.total_marks() << endl;
     return 0;
 }

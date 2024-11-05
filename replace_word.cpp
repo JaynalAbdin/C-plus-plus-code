6d874a55b97e90d79fb7ebfe3f6d336744a18928
Md. Jaynal Abdin <mjabdin.311@gmail.com>
2024-11-05 11:23:26 +0600

    Replaced word added successfully

--- replace_word.cpp
+++ replace_word.cpp
@@ -6,6 +6,11 @@ int main()
     string s;
     getline(cin, s);
     // s.replace(0, "")
+    while (s.find("EGYPT") != -1)
+    {
+        s.replace(s.find("EGYPT"), 5, " ");
+    }
+    cout << s << endl;
 
     return 0;
 }
